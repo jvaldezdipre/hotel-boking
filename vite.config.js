afb8f59c9148e6e3f14b57ddfa44a2d6ffeb8d61
Jeffrey Valdez <jeff.valdez321@outlook.com>
2025-03-31 15:46:38 -0400

    deploying to render

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hotel-boking/",
+  //base: "/hotel-boking/",
   test: {
     environment: "jsdom",
     globals: true,
